d81175bd4278cc982fd0e74b03c59c77cb9cc25c
Rafael Gutiérrez <rafagutibarrio@gmail.com>
2026-06-07 14:50:59 -0600

    verifications: add pre authorize method to verifications endpoints

--- ecommerce/src/main/java/com/uca/ecommerce/controller/VerificationController.java
+++ ecommerce/src/main/java/com/uca/ecommerce/controller/VerificationController.java
@@ -9,7 +9,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.UUID;
 
@@ -20,11 +22,13 @@ public class VerificationController extends BaseController {
 
     private final VerificationService verificationService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/")
     public ResponseEntity<GeneralResponse> getAllVerifications() {
         return buildResponse("Verifications retrieved successfully", HttpStatus.OK, verificationService.getAllVerifications());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<GeneralResponse> getVerificationById(@PathVariable Long id) {
         return buildResponse("Verification retrieved successfully", HttpStatus.OK, verificationService.getVerificationById(id));
@@ -35,26 +39,31 @@ public class VerificationController extends BaseController {
         return buildResponse("Verifications retrieved successfully", HttpStatus.OK, verificationService.getVerificationsByProductId(productId));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/verified-by/{verifiedByUuid}")
     public ResponseEntity<GeneralResponse> getVerificationsByVerifiedBy(@PathVariable UUID verifiedByUuid) {
         return buildResponse("Verifications retrieved successfully", HttpStatus.OK, verificationService.getVerificationsByVerifiedBy(verifiedByUuid));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<GeneralResponse> createVerification(@Valid @RequestBody CreateVerificationRequest request) {
         return buildResponse("Verification created successfully", HttpStatus.CREATED, verificationService.createVerification(request));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<GeneralResponse> updateVerification(@Valid @RequestBody UpdateVerificationRequest request, @PathVariable Long id) {
         return buildResponse("Verification updated successfully", HttpStatus.OK, verificationService.updateVerification(request, id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/patch/{id}")
     public ResponseEntity<GeneralResponse> patchVerification(@Valid @RequestBody PatchVerificationRequest request, @PathVariable Long id) {
         return buildResponse("Verification patched successfully", HttpStatus.OK, verificationService.patchVerification(request, id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<GeneralResponse> deleteVerification(@PathVariable Long id) {
         return buildResponse("Verification deleted successfully", HttpStatus.OK, verificationService.deleteVerification(id));
