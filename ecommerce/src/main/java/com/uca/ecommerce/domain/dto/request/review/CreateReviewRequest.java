5e129267bdc807d3cd9e8cb0bdcdc0bc214c845b
Rafael Gutiérrez <rafagutibarrio@gmail.com>
2026-06-25 15:32:49 -0600

    fix reviews security

--- ecommerce/src/main/java/com/uca/ecommerce/domain/dto/request/review/CreateReviewRequest.java
+++ ecommerce/src/main/java/com/uca/ecommerce/domain/dto/request/review/CreateReviewRequest.java
@@ -20,9 +20,6 @@ public class CreateReviewRequest {
     @NotNull(message = "Product ID is required")
     private UUID productId;
 
-    @NotNull(message = "User ID is required")
-    private UUID userId;
-
     @NotNull(message = "Rating is required")
     @Min(value = 1, message = "Rating must be at least 1")
     @Max(value = 5, message = "Rating must be at most 5")
